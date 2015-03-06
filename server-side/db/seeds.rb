7809879f9437d8dd63802226f1586b5d6ff4413e
vineetrastogi <vineetrastogi@gmail.com>
2015-03-05 21:26:21 -0800

    Seeding works for HTTParty post request. Was able to make one API call from seed file.  Need to seed remaining trip objects

--- server-side/db/seeds.rb
+++ server-side/db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'httparty'
+
+request = {
+  "request" => {
+    "slice"=> [
+      {
+        "origin" => "SFO",
+        "destination"=> "SEA",
+        "date"=> "2015-04-29"
+      }
+    ],
+    "passengers"=> {
+      "adultCount"=> 1
+    },
+    "solutions"=> 1,
+    "refundable"=> false
+  }
+}.to_json
+
+response = HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyB9sDMOUjCNYYrn4K0A_CxPmEF7v2k741g",
+body: request,
+headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+
+p response
