b48fe6edfbae15049b1b5dea233528b3c0125ac6
vineetrastogi <vineetrastogi@gmail.com>
2015-03-08 21:32:02 -0700

    bug fixed: only the results pertaining to the input are displayed

--- server-side/app/controllers/trips_controller.rb
+++ server-side/app/controllers/trips_controller.rb
@@ -5,7 +5,7 @@ class TripsController < ApplicationController
 include DataParserHelper # SEE HELPERS DIRECTORY
 
   def index
-    airport_codes =  %w(LAX SEA AUS BWI BOS CLT MDW ORD CVG CLE )
+    airport_codes =  %w(LAX ORD CVG CLE CMH DFW DEN)
       # AUS BWI BOS CLT MDW ORD CVG CLE CMH DFW DEN DTW FLL RSW BDL HNL IAH HOU IND MCI LAS LAX MEM MIA MSP BNA MSY JFK LGA EWR OAK ONT MCO PHL PHX PIT PDX RDU SMF SLC SAT SAN SJC SNA SEA STL TPA IAD DCA)
     original_airport_codes = airport_codes.clone
     airport_codes.delete(params['origin'])
@@ -33,36 +33,27 @@ include DataParserHelper # SEE HELPERS DIRECTORY
         @response = HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=#{ENV['GOOGLE_API_TOKEN']}",
         body: request,
         headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+counter = 0
+            # ERROR HANDLING: Ensuring that api_call is returning a response with respect to the user's input
+            if @response['trips']['data'].size < 2
+              @invalid_input = "No flights found with provided inputs. Please consider a different date or budget."
+            else
+              @duration = @response['trips']['tripOption'][counter]['slice'][0]['duration']
+              @depart_time = @response['trips']['tripOption'][counter]['slice'][0]['segment'].first['leg'][0]['departureTime']
+              @arrival_time = @response['trips']['tripOption'][counter]['slice'][0]['segment'].last['leg'][0]['arrivalTime']
+              @carrier = @response['trips']['data']['carrier'][counter]['name']
+              @sale_total = @response['trips']['tripOption'][counter]['saleTotal'].reverse.chomp('DSU').reverse.to_f
+              @carrier_code = @response['trips']['tripOption'][0]['slice'][0]['segment'][counter]['flight']['carrier']
+              @flight_number = @response['trips']['tripOption'][0]['slice'][0]['segment'][counter]['flight']['number']
+              @origin = find_city(params['origin'])
+              @destination_code = airport
+              @destination = find_city(airport)
 
-        # while counter < airport_codes.length
-        # 2.times do
-
-        counter = 0
-          # ERROR HANDLING
-          if @response['trips']['data'].size < 2
-            @invalid_input = "No flights found with provided inputs. Please consider a different date or budget."
-          else
-            @duration = @response['trips']['tripOption'][counter]['slice'][0]['duration']
-            @depart_time = @response['trips']['tripOption'][counter]['slice'][0]['segment'].first['leg'][0]['departureTime']
-            @arrival_time = @response['trips']['tripOption'][counter]['slice'][0]['segment'].last['leg'][0]['arrivalTime']
-            @carrier = @response['trips']['data']['carrier'][counter]['name']
-            @sale_total = @response['trips']['tripOption'][counter]['saleTotal'].reverse.chomp('DSU').reverse.to_f
-            @carrier_code = @response['trips']['tripOption'][0]['slice'][0]['segment'][counter]['flight']['carrier']
-            @flight_number = @response['trips']['tripOption'][0]['slice'][0]['segment'][counter]['flight']['number']
-            # # p @mileage =
-            @origin = find_city(params['origin'])
-            @destination_code = airport
-            @destination = find_city(airport)
-
-              # counter += 1
-              # end
-            # end
-
-            Trip.create(sale_total: @sale_total, carrier: @carrier, carrier_code: @carrier_code, flight_number: @flight_number, depart_time: @depart_time, arrival_time: @arrival_time, duration: @duration, mileage: @mileage, origin: @origin, destination: @destination, destination_code: @destination_code)
-          end
+              Trip.create(sale_total: @sale_total, carrier: @carrier, carrier_code: @carrier_code, flight_number: @flight_number, depart_time: @depart_time, arrival_time: @arrival_time, duration: @duration, mileage: @mileage, origin: @origin, destination: @destination, destination_code: @destination_code)
+            end
         airport_codes = original_airport_codes
     end
-    @trips = Trip.all.order(sale_total: 'asc')
+    @trips = Trip.where(origin: @origin)
     @client_side = {trips: @trips, invalid_input: @invalid_input}
     render json: @client_side
   end
