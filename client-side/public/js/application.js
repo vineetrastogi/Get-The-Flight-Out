4f19afe0a183c1329dba0c69c476bef15da8c595
vineetrastogi <vineetrastogi@gmail.com>
2015-03-10 14:32:02 -0700

    removed uneccessary codes

--- client-side/public/js/application.js
+++ client-side/public/js/application.js
@@ -26,21 +26,7 @@ function eventListeners() {
 
     // uncomment below in order to make ajax post request
     // leads to replaceSearchBox and populateResultsTemp
-    // add retDate?
     submitRequest(origin, budget, depDate, retDate);
-
-    // to test fade in and fade out
-    // replaceSearchBox();
-
-    // to test handlebars template
-    //LEGIT FLIGHT!!!
-    // var data = {
-    //   trips: [
-    //   {"index": 0, "sale_total": "80", "carrier": "Virgin America", "carrier_code": "VX", "flight_number" :"906", "depart_time" :"2015-03-23 at 9:00", "origin":"SFO" , "arrival_time":"2015-03-27 at 10:00" , "destination":"LAS" },
-    //   {"index": 1, "sale_total": "1800", "carrier": "United Airlines", "carrier_code": "UA", "flight_number" :"990", "depart_time" :"2015-03-13 at 9:00", "origin":"SFO" , "arrival_time":"2015-03-27 at 10:00" , "destination":"CDG"},
-    //   {"index": 2, "sale_total": "900", "carrier": "United Airlines", "carrier_code": "UA", "flight_number" :"837", "depart_time": "2015-03-16 at 9:00", "origin":"SFO" , "arrival_time":"2015-03-18 at 10:00" , "destination":"NRT"}]
-    // };
-    // populateResultsTemp(data, origin, retDate);
   }); //end of on click
 }
 
@@ -75,13 +61,8 @@ function submitRequest(origin, budget, depDate, retDate) {
 function replaceSearchBox() {
   console.log("in replaceSearchBox");
 
-  // fadeOut
-  // $(".search-bar-wrapper").animate({ opacity:0 }, fadeInTextBox());
-
   //fadein
-  // function fadeInTextBox() {
     $(".result-text").fadeIn("slow");
-  // }
 }
 
 // called in submitRequest callback when successful
@@ -89,22 +70,36 @@ function populateResultsTemp(data, origin, retDate) {
   console.log("in populateResultsTemp");
   console.log(data, origin, retDate);
 
-  // FOR TESTING ONLY
-  // var depDate = depDate;
-  //   depDate = "REPLACE THIS";
-  // $('.date-time#departure').children().text(depDate);
-
   var source = $("#results-template").html();
   var template = Handlebars.compile(source);
-    // debugger;
-    var context = data.trips;
+  var context = data.trips;
 
-    $(".results-wrapper").html(template(context));
+  $(".results-wrapper").html(template(context));
 
   // directs on "click" event listener to redirect user to googleflights ticket purchse
   redirectToPurchase(context, origin, retDate);
   trackDataViaEmail();
+}
 
+function redirectToPurchase(context, origin, retDate) {
+  $(".button#purchase").on("click", function(event) {
+    event.preventDefault();
+
+    console.log('in redirectToPurchase');
+    console.log("*************************");
+
+    // grab data attribute value of button that was clicked
+    var indexString = $(this).attr("data");
+    var index = parseInt(indexString);
+    var departDate = context[index].depart_time.substring(0,10);
+    console.log(context);
+
+    var purchaseLink = "https://www.google.com/flights/#search;f="+origin+";t="+context[index].destination_code+";d="+departDate+";r="+retDate+";sel=*";
+
+    console.log(purchaseLink);
+
+    window.open(purchaseLink);
+  });
 }
 
 // POP UP WINDOW FOR USER TO INSERT NAME AND EMAIL TO RECEIVE PUSH NOTIFICATION
@@ -112,15 +107,12 @@ function trackDataViaEmail() {
   $('#parent-container').on('click', '.email-button',  function(event){
     var clickedElement = event.target.id
     event.preventDefault();
-    // var trackButton = $(this).attr('data')
     console.log(clickedElement);
-    alert('hi')
     formDialog(clickedElement);
   });
 }
 
 function formDialog(clickedElement) {
-  console.log('form dialog')
   $("#dialog").dialog({
     autoOpen: false
   });
@@ -143,7 +135,6 @@ function sendEmail(clickedElement) {
       console.log('success');
       console.log(response);
       $('#dialog').parent().remove();
-      // $('#dialog').parent().css("display", "none");
     })
     .fail(function() {
       console.log("error");
@@ -151,40 +142,5 @@ function sendEmail(clickedElement) {
   });
 }
 
-// Validating Form Fields.....
-// $("#submit").click(function(e) {
-//   var email = $("#email").val();
-//   var name = $("#name").val();
-//   var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-//   if (email === '' || name === '') {
-//     alert("Please fill all fields...!!!!!!");
-//     e.preventDefault();
-//   } else if (!(email).match(emailReg)) {
-//     alert("Invalid Email...!!!!!!");
-//     e.preventDefault();
-//   } else {
-//     alert("Form Submitted Successfully......");
-//   }
-// });
 
 
-function redirectToPurchase(context, origin, retDate) {
-  $(".button#purchase").on("click", function(event) {
-    event.preventDefault();
-
-    console.log('in redirectToPurchase');
-    console.log("*************************");
-
-    // grab data attribute value of button that was clicked
-    var indexString = $(this).attr("data");
-    var index = parseInt(indexString);
-    var departDate = context[index].depart_time.substring(0,10);
-    console.log(context);
-
-    var purchaseLink = "https://www.google.com/flights/#search;f="+origin+";t="+context[index].destination_code+";d="+departDate+";r="+retDate+";sel=*";
-
-    console.log(purchaseLink);
-
-    window.open(purchaseLink);
-  });
-}
