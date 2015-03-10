6fbb873468528afa476af63299d9f42a5904d8b9
vineetrastogi <vineetrastogi@gmail.com>
2015-03-10 16:59:48 -0700

    able to pass url,name, and email from javascript and forms to Ruby User Objects

--- server-side/app/controllers/users_controller.rb
+++ server-side/app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(name: params['name'], email: params['email'])
+    p '$' * 100
+    p params
+    p '$' * 100
+    @user = User.create(name: params['formData']['0']['value'], email: params['formData']['1']['value'], link: params['purchaseLinkForEmail'])
     render json: @user
   end
 end
