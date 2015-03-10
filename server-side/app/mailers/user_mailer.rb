84f8a5905a21f9edd9834e7ca582471aa1096f15
vineetrastogi <vineetrastogi@gmail.com>
2015-03-09 20:22:45 -0700

    added users table to migration and user model

--- server-side/app/mailers/user_mailer.rb
+++ server-side/app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url =
+    @url = 'http://localhost:9393/'
+    mail(to: @user.email, subject: 'What rhymes with ornages?')
   end
 end
