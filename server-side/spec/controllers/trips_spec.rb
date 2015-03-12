02769700d0ed3b2f10db5e9be7a312f0f9863ba9
Amir <beheshtaein.a@gmail.com>
2015-03-11 19:40:24 -0700

    added test

---
+++ server-side/spec/controllers/trips_spec.rb
@@ -0,0 +1,16 @@
+# require 'spec_helper'
+# require 'rails_helper'
+
+# RSpec.describe TripsController, type: :controller do
+
+#   describe '#index' do
+#     it 'responds with a 200' do
+#       post "http://localhost/3000/"
+#       p '*' * 100
+#       p response
+#       expect(response.status).to eq(200)
+#     end
+#   end
+
+# end
+
